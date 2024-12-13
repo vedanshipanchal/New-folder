635808d48f1f60753fde29f196ac80d461f73999
vedanshipanchal <vedanshipanchal@gmail.com>
2024-12-13 17:48:05 +0000

    square root

---
+++ main.py
@@ -0,0 +1,10 @@
+#find the square root
+number =("Enter a number to find it's square root:")
+
+#if the number is non-negative
+if number >0:
+
+    square_root = ()
+    print(f"the square root of number is square_root")
+else:
+    print("it's not a square root.")
